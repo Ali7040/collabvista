3f55430c65b91aef6bd1068a33144967a7ae09f4
Ali Haider <ha4219466@gmail.com>
2024-09-13 19:00:56 +0500

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,22 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
-      animation: {
-        background1: "background1 10s linear infinite",
-      },
-      keyframes: {
-        background1: {
-          "0%": { backgroundPosition: "50% 0%" },
-          "50%": { backgroundPosition: "50% 100%" },
-          "100%": { backgroundPosition: "50% 0%" },
-        },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
       },
       boxShadow: {
         "custom-dark": "16px 16px 0 #DBAFA0", // Custom brown shadow
