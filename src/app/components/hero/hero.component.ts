60e0b403bcf5dddd432388f84f5599547358fe3e
Ali Haider <ha4219466@gmail.com>
2024-09-15 19:11:58 +0500

    update hero component

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { SearchComponent } from '../../shared/search/search.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [HeaderComponent, SearchComponent],
+  imports: [HeaderComponent, SearchComponent, AboutComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss',
 })
