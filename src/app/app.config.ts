6e27b11f0eb47e89dc70e781fcfdde44b4355d75
Ali Haider <ha4219466@gmail.com>
2024-09-22 22:33:56 +0500

    Update .gitignore to exclude environment files

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 import { routes } from './app.routes';
-import { environment } from '../env/env';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
