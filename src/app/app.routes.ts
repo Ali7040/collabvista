3bee1d8aeee59dee4a0a6a511f3d84fb112769fb
Ali Haider <ha4219466@gmail.com>
2024-09-24 23:00:45 +0500

    new component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { LoginComponent } from './auth/login/login.component';
 
-export const routes: Routes = [
-  {
-    path: 'sign-up',
-    component: SignUpComponent,
-  },
-  {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: '',
-    component: HeroComponent,
-  },
-];
+export const routes: Routes = [];
